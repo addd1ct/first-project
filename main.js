5a7a514dbb1b92b0b0f24b2f5d5ddab723859cdb
polly <polifibliss@gmail.com>
2025-02-05 22:33:50 +0200

    update

--- main.js
+++ main.js
@@ -1,13 +1,2 @@
 import Accordion from "accordion-js";
 import "accordion-js/dist/accordion.min.css";
-
-document.addEventListener("DOMContentLoaded", function () {
-    const accordion = new Accordion(".faq-container", {
-        duration: 300,
-        showMultiple: false,
-    });
-
-    document.querySelector(".faq-btn").addEventListener("click", function () {
-        this.classList.button("active");
-    });
-});
